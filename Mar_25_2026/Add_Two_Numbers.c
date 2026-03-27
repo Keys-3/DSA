24b828d386dc2a4b582b25b074cf79b50ba92212
keys-3 <140890501+Keys-3@users.noreply.github.com>
2026-03-27 10:32:43 +0530

    March 25-26

---
+++ Mar_25_2026/Add_Two_Numbers.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+struct ListNode* createNode(int val) {
+    struct ListNode* node = (struct ListNode*)malloc(sizeof(struct ListNode));
+    node->val = val;
+    node->next = NULL;
+    return node;
+}
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;
+    struct ListNode* curr = &dummy;
+    dummy.next = NULL;
+    int carry = 0;
+    while(l1 != NULL || l2 != NULL || carry) {
+        int sum = carry;
+        if(l1) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if(l2) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        carry = sum / 10;
+        curr->next = createNode(sum % 10);
+        curr = curr->next;
+    }
+    return dummy.next;
+}
---
+++ Mar_26_2026/Reorder_List.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+struct ListNode* reverse(struct ListNode* head) {
+    struct ListNode *prev = NULL, *curr = head;
+
+    while(curr) {
+        struct ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+void reorderList(struct ListNode* head) {
+    if(!head || !head->next) return;
+    struct ListNode *slow = head, *fast = head;
+    while(fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    struct ListNode* second = reverse(slow->next);
+    slow->next = NULL;
+    struct ListNode* first = head;
+    while(second) {
+        struct ListNode* temp1 = first->next;
+        struct ListNode* temp2 = second->next;
+        first->next = second;
+        second->next = temp1;
+        first = temp1;
+        second = temp2;
+    }
+}
